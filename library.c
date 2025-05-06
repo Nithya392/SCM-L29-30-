e6a0a5c9a8fd59de3b3c76b94ad5874c8a7415aa
Nithya392 <nithyashree.b@s.amity.edu>
2025-05-06 23:57:52 +0530

    commited

--- library.c
+++ library.c
@@ -3,7 +3,6 @@
 struct Book {
     char title[100];
     char author[50];
-    float price;
     int year;
 };
 int main() {
@@ -11,21 +10,14 @@ int main() {
 
     printf("Enter Book Title :\n ");
     scanf(" %s", b.title);
-
     printf("Enter Author Name :\n ");
     scanf(" %s", b.author);
-
-    printf("Enter Book Price :\n ");
-    scanf("%f", &b.price);
-
     printf("Enter Published year :\n");
     sacnf("%d" , &b.year);
 
     printf("\nBook Details :\n");
     printf("Title : %s\n", b.title);
     printf("Author: %s\n", b.author);
-    printf("Price : %.2f\n", b.price);
-
     return 0;
 }
 
