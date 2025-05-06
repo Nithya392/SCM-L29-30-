a6d894b5ccc586ecfd01196a9a5f15dd4d86686f
Nithya392 <nithyashree.b@s.amity.edu>
2025-05-06 23:40:57 +0530

    This is to take user input

--- library.c
+++ library.c
@@ -5,3 +5,15 @@ struct Book {
     char author[50];
     float price;
 };
+int main() {
+    struct Book b;
+
+    printf("Enter Book Title :\n ");
+    scanf(" %s", b.title);
+
+    printf("Enter Author Name :\n ");
+    scanf(" %s", b.author);
+
+    printf("Enter Book Price :\n ");
+    scanf("%f", &b.price);
+
